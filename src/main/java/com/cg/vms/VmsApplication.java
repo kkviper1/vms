9d64a815927155a7266a6b9368537f815255cc12
kkviper1 <kkviper1@gmail.com>
2021-05-11 14:05:28 +0530

    servlet

--- src/main/java/com/cg/vms/VmsApplication.java
+++ src/main/java/com/cg/vms/VmsApplication.java
@@ -2,9 +2,10 @@ package com.cg.vms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VmsApplication {
+public class VmsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(VmsApplication.class, args);
